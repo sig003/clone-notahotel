65c8ed6eca7824ce7e9c402a82604c75259d0ad8
sig03 <sig03@naver.com>
2022-08-30 07:21:05 +0000

    set layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,16 +1,33 @@
 import styled from 'styled-components';
 
+interface ChildrenProps {
+  children: React.ReactNode;
+}
+
 const Wrapper = styled.div`
   width: 100%;
   height: auto;
   border: 1px solid red;
+  padding-top: 60px;
 `;
 
-export default function Layout() {
+const Header = styled.div`
+  width: 100%;
+  position: fixed;
+  height: 60px;
+  border:1px solid blue;
+  top: 0;
+  left: 0;
+  right: 0;
+  background-color: hsl(0, 0%, 100%);
+`;
+
+export default function Layout({ children }: ChildrenProps) {
   return (
     <>
       <Wrapper>
-        aaa
+        <Header>header</Header>
+        {children}
       </Wrapper>
     </>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,12 @@
 import type { NextPage } from 'next';
 import styled from 'styled-components';
 
-const Wrapper = styled.div`
-  width: 100%;
-  height: auto;
-  border: 1px solid red;
-`;
-
 const Home: NextPage = () => {
   return (
     <>
-      <Wrapper>
-        aa
-      </Wrapper>
+      <div>
+        bb
+      </div>
     </>
   )
 }
