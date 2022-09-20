a94f59cb9fac0fa5247ae8660f0ea5056e4417f5
sig03 <sig03@naver.com>
2022-09-20 00:14:21 +0000

    add css styles

--- src/components/SectionFeatures/SectionFeatures.tsx
+++ src/components/SectionFeatures/SectionFeatures.tsx
@@ -1,4 +1,7 @@
 import styled from 'styled-components';
+import AOS from 'aos';
+import "aos/dist/aos.css";
+import { useEffect } from 'react';
 
 const SectionTag = styled.section`
   width: 700px;
@@ -13,12 +16,12 @@ const SectionTag = styled.section`
 `;
 
 const TopPTag = styled.p`
-  opacity: 1;
-  transform: translate(0px, 0px);
+  //opacity: 1;
+  //transform: translate(0px, 0px);
   font-size: 34px;
   margin-top: 0px;
   font-family: "Gothic MB101 Bold", "Gothic MB101 Medium", -apple-system, "Segoe UI", Roboto, "Droid Sans", sans-serif;
-  font-weight: bold;
+  font-weight: normal;
   line-height: 1.42857143;
   text-align: left;
   display: block;
@@ -29,8 +32,8 @@ const TopPTag = styled.p`
 `;
 
 const BottomPTag = styled.p`
-  opacity: 1;
-  transform: translate(0px, 0px);
+  //opacity: 1;
+  //transform: translate(0px, 0px);
   font-size: 16px;
   line-height: 2;
   margin-top: 35px;
@@ -45,12 +48,17 @@ const BottomPTag = styled.p`
 `;
 
 export default function SectionFeatures() {
+  useEffect(() => {
+    AOS.init();
+  });
   return (
     <>
+    <div data-aos="fade-up">
       <SectionTag>
         <TopPTag>Buy a vacation home that doubles <br />as a hotel</TopPTag>
         <BottomPTag>You can use it as a personal vacation home. When you aren't using it, NOT A HOTEL will rent it out. You can buy as many as you want, giving you many homes. We provide an ideal home life. </BottomPTag>
       </SectionTag>
+      </div>
     </>
   );
 }
